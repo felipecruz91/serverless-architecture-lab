9c484ab562fc88190455a11db7252f2f3b2ef2b6
Felipe Cruz Martinez <felipecruz91@hotmail.es>
2018-03-01 15:13:54 +0100

    Finished the ExportLicensePlates function

--- TollBooth/DatabaseMethods.cs
+++ TollBooth/DatabaseMethods.cs
@@ -38,11 +38,12 @@ namespace TollBooth
 
             using (_client = new DocumentClient(new Uri(_endpointUrl), _authorizationKey))
             {
-                // MaxItemCount value tells the document query to retrieve 100 documents at a time until all are returned.
-                // TODO 5: Retrieve a List of LicensePlateDataDocument objects from the collectionLink where the exported value is false.
-                // COMPLETE: licensePlates = _client.CreateDocumentQuery ...
-                // TODO 6: Remove the line below.
-                licensePlates = new List<LicensePlateDataDocument>();
+              // MaxItemCount value tells the document query to retrieve 100 documents at a time until all are returned.
+              // TODO 5: Retrieve a List of LicensePlateDataDocument objects from the collectionLink where the exported value is false.
+              licensePlates = _client
+                .CreateDocumentQuery<LicensePlateDataDocument>(collectionLink, new FeedOptions {MaxItemCount = 100})
+                .Where(l => l.exported == false)
+                .ToList();
             }
 
             exportedCount = licensePlates.Count();
--- TollBooth/FileMethods.cs
+++ TollBooth/FileMethods.cs
@@ -58,7 +58,7 @@ namespace TollBooth
                         // Upload blob.
                         stream.Position = 0;
                         // TODO 7: Asyncronously upload the blob from the memory stream.
-                        // COMPLETE: await blob...;
+                        await blob.UploadFromStreamAsync(stream);
 
                         successful = true;
                     }
